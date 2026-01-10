ffd874be7ce983b7c42a62e0e1c56f3f8ac88867
Bill2926 <nguyenducmanh2926@gmail.com>
2026-01-10 23:28:20 +0700

    Week 1 Initial
    nothing much for now.

---
+++ W1/Project1/vocvach.cpp
@@ -0,0 +1,116 @@
+//// Header files library
+//#include <iostream>
+//#include <string>
+//#include <cmath>
+//
+//// Standard (std) namespace
+//using namespace std;
+//
+//const int USD = 24000;
+//
+//
+//class Book {
+//public:
+//	string title;
+//	string author;
+//	int year;
+//};
+//
+//
+//int main() {
+//	// "<<": insertion operator
+//	/*cout << "Hello World!\n" << "\n";
+//	cout << 3 << "\n";*/
+//	
+//	/*mutiple lines
+//	comments*/
+//
+//	//int testingNum = 10;
+//	//string vcl = "My testing num is ";
+//	//cout << vcl << testingNum << "!" << endl;
+//
+//	///*int x, y, z;
+//	//x = y = z = 10;
+//	//int k = x + y + z;
+//	//cout << k;*/
+//	//cout << 48000 / USD;
+//
+//	/*int userInput;
+//	string userName;
+//
+//	cout << "Please enter your name: ";
+//	cin >> userName;
+//
+//	cout << "Hello " << userName << "!" << endl;
+//	cout << "Enter your money in VND: ";
+//	cin >> userInput;
+//
+//	int result = userInput / USD;
+//
+//	cout << "You have " << result << "$ in your pocket!" << endl;*/
+//
+//	//string test1 = "Thuan ";
+//	//string test2 = "Minh";
+//	//// String is also an OBJECT in C++, therefore it also has functions
+//	//cout << test1.append(test2) << endl;
+//	//cout << test1.at(1)<< endl;
+//
+//	///*string name;
+//	//getline(cin, name);
+//	//cout << "Your full name is: " << name << endl;*/
+//	//cout << log(4);
+//
+//
+//	//// boolalpha to manipulate cout to print the whole words of boolean
+//	//// noboolapla to end it
+//	//bool x = false;
+//	//cout << boolalpha << endl;
+//	//cout << x;
+//	//cout << noboolalpha << endl;
+//	//cout << x;
+//	
+//
+//	// Structure: a group of relatable vars
+//	/*struct {
+//		string name;
+//		int age;
+//		float height;
+//	} HumanStruct;
+//
+//	HumanStruct.name = "Bill";
+//	HumanStruct.age = 19;
+//	HumanStruct.height = 1.75;
+//
+//	cout << HumanStruct.name << endl;
+//	cout << HumanStruct.age << endl;
+//	cout << HumanStruct.height << endl;
+//
+//
+//	enum lang {
+//		LOW = 10,
+//		HIGH = 20,
+//		OUTDATED = 5
+//	};
+//
+//	lang testVar = OUTDATED;
+//	cout << testVar << endl;*/
+//
+//	/*string bev = "avo";
+//	string* ptr = &bev;
+//
+//	cout << ptr << endl;*/
+//
+//	Book book_obj_1;
+//	book_obj_1.title = "Maltida";
+//	book_obj_1.author = "Roald Dahl";
+//	book_obj_1.year = 1988;
+//
+//	Book book_obj_2;
+//	book_obj_2.title = "The Giving Tree";
+//	book_obj_2.author = "Shel Silverstein";
+//	book_obj_2.year = 1964;
+//
+//	cout << book_obj_1.title << ", " << book_obj_1.author << ", " << book_obj_1.year;
+//	// End the function
+//	return 0;
+//}
---
+++ W1/test.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Thư viện nhập/xuất cơ bản
+
+int main() {
+    std::cout << "Hello, World!" << std::endl; // In ra màn hình
+    return 0; // Kết thúc chương trình thành công
+}
